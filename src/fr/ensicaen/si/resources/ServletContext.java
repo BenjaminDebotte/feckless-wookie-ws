4c70cfe6d838acc281954b81c2d575ce1ef5fc75
Benjamin Debotté <benjamin.debotte@ecole.ensicaen.fr>
2015-11-26 17:44:26 +0100

    Ajout du fichier web.xml compatible. Ajout de la classe ServletContext

---
+++ src/fr/ensicaen/si/resources/ServletContext.java
@@ -0,0 +1,19 @@
+package fr.ensicaen.si.resources;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ServletContext implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
