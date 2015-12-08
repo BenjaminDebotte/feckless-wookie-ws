d04ac30354b6422412048942ba1794df1752367a
Benjamin Debotté <benjamin.debotte@ecole.ensicaen.fr>
2015-12-08 17:05:13 +0100

    Ajout de fichiers pour la gestion réseau

--- src/fr/ensicaen/si/init/ServletContext.java
+++ src/fr/ensicaen/si/init/ServletContext.java
@@ -10,19 +10,16 @@ import db.DbManagement;
 public class ServletContext implements ServletContextListener {
 
 	@Override
+	//serveur-apprentissage.ensicaen.fr/
+	//172.28.106.1:8080    manager:manager
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("PENIS");
+		//DbManagement.getInstance().connexion("jdbc:mysql://localhost/si/");
+	}
+	
+	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		DbManagement.getInstance().deconnexion();
 	}
-
-	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
-		/* Insérer ici toutes les initialisations */
-		try {
-			DbManagement.getInstance().connexion("");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
+	
 }
--- src/fr/ensicaen/si/resources/ClientsResources.java
+++ src/fr/ensicaen/si/resources/ClientsResources.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -39,16 +40,18 @@ public class ClientsResources {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getCount() {
 		String countStr = "";
-		try {
-			countStr = "10";
-			//countStr = new Integer(ClientDao.getInstance().countClient()).toString();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		countStr = "10";
+		//countStr = new Integer(ClientDao.getInstance().countClient()).toString();
 		
 		return countStr;
 		
+	}
+	
+	
+	@Path("id/{clientid}")
+	public ClientResources getClientById(@PathParam("clientid") int id) {
+		//return new ClientResources(uriInfi, request, id);
+		return new ClientResources();
 	}
 	
 }
